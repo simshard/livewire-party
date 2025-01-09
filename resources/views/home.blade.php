f1680bc8d68dbf402404bf0a7e7562528a2ce9b7
Sim <simshard@gmail.com>
2025-01-09 19:02:32 +0000

    initial setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Livewire Demo App</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased font-sans">
+
+        <h1 class="mt-8 text-3xl text-center font-cursive text-slate-800">🫶 Livewire Demo App</h1>
+    <livewire:dashboard lazy />
+
+    </body>
+</html>
---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Volt\Component;
+
+new class extends Component {
+    //
+}; ?>
+
+<div class="flex items-center justify-center h-screen">
+    <div class="bg-blue-200 text-green-700 p-6 rounded-lg w-2/3 h-2/3" >
+        Hello Simon
+    </div>
+</div>
